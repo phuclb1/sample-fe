e454534d6a473f74068d1d6b9edce349b35a1671
Phuc Le <phuclb1@gmail.com>
2022-01-04 21:04:01 +0700

    fix typo

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
-var NXT_API_BASE_URL = "https://api-services.nextg.team";
+var NXT_API_BASE_URL = "https://api-services.nextg.team/";
 var NXT_RESOURCE_URL = "https://api-services.nextg.team";
 //var NXT_API_BASE_URL = "";
 var NXT_TIMMER_DEFAULT = 10;
